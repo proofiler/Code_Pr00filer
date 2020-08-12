6f5984e41ee02b2b18f806682113351ffc15f800
Eidorian <32389491+RedFou52@users.noreply.github.com>
2020-08-12 13:46:17 +0200

    Update echo & Install clamav

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Script d'installation de la station blanche
-echo "update du système"
+echo "[+] update du système"
 sudo apt update -y
 sudo apt upgrade -y
 
 # PyQT5
-echo "Installation de PyQt5"
+echo "[+] Installation de PyQt5"
 sudo apt-get install build-essential checkinstall -y
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev -y
 sudo apt-get install python3-pyqt5 -y
@@ -14,39 +14,41 @@ sudo apt-get install pyqt5-dev-tools -y
 sudo apt-get install qttools5-dev-tools -y
 
 # Python3 libraries
-echo "Installation des librairies Python3"
+echo "[+] Installation des librairies Python3"
 pip3 install sip
 pip3 install --upgrade pip
 pip3 install requests
 
 # Création du dossier pi dans media avec les bons droits
-echo "Configuration de /media"
+echo "[+] Configuration de /media"
 cd /media
 sudo mkdir pi
 sudo chmod a+rwx pi/
 
 # Installation du Code avec GIT
-echo "Installation de Pr00filer"
+echo "[+] Installation de Pr00filer"
 cd /opt/
 sudo git clone https://github.com/Pr00filer/Code_Pr00filer.git
 sudo chown -R pi Code_Pr00filer
 
 # Installation de clamav 
-echo "Installation de clamav"
-sudo apt install clamav -y
+echo "[+] Installation de clamav"
+sudo apt install clamav clamdscan clamav-daemon -y
 sudo killall freshclam
 sudo freshclam
+sudo service clamav-daemon start
 
 # Creation de la règle UDEV
-echo "Creation de la règle UDEV"
+echo "[+] Creation de la règle UDEV"
 sudo cp /opt/Code_Pr00filer/doc/11-insert.rules /etc/udev/rules.d/11-insert.rules
 sudo cp /opt/Code_Pr00filer/doc/11-remove.rules /etc/udev/rules.d/11-remove.rules
 sudo udevadm control --reload
 
 # Création du SERVICE insertUSB
-echo "Creation du SERVICE insertUSB"
+echo "[+] Creation du SERVICE insertUSB"
 sudo cp /opt/Code_Pr00filer/doc/insertUSB.service /etc/systemd/system/insertUSB.service
 sudo systemctl enable insertUSB.service
 sudo service insertUSB start
 
+echo "[+] Fin du script"
 
