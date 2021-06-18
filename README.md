38794b6d99a0a038db9ea27178530c970b34994e
AlrikRr <32389491+AlrikRr@users.noreply.github.com>
2021-06-19 00:21:25 +0200

    Update README + Comments main.py: 2021-06-19 00:21:25

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 # Pr00filer - Station Blanche / Station de décontamination des clés USB
 
-## À savoir :
-Ce projet est toujours en cours de développement.  
-Les images de sensibilation utilisée pour le moment proviennent du projet [USBGuardian](https://github.com/USBGuardian/USBGuardian) afin de servir d'exemple.
 
+## À savoir :
+Ce projet a été réalisé pour notre projet final d'études.
+Il est fontionnel mais pas optimal, vous etes libre d'y contribuer afin de l'améliorer.
 
 ## Contexte :
 
-Réalisation d'une station de décontamination automatique des clés USB.  
+Réalisation d'une station de décontamination automatique des clés USB.
+Il y a la possibilité d'ajouter un serveur qui fera office de master pour controler plusieurs station.
 
-- Vérification des fichiers malveillant avec l'anti-virus clamAV.
-- Vérification des hashs de fichiers avec VirusTotal.
-- Vérification des extensions des fichiers pr"sent sur la clé USB.
+**Code du serveur** : https://github.com/proofiler/Proofiler_web
+
+Fonctionnement Raspberry : 
+- Monte la clé automatiquement et lance l'application graphique.
+- Supprime les fichiers qui ont une extensions interdite dans le fichier de configuration.
+- Supprime les fichiers qui ont le même hash déjà détecté comme malveillant par d'autres scan.(Fonctionnalité disponible avec le master)
+- Analyse anti-virus avec **ClamAV** ( Utilisation de daemon pour une analyse rapide)
+- Vérification et suppression des fichiers avec un check sur **VirusTotal** ( Limité à un certains nombres de requêtes)
+- Vérification et suppression des fichiers qui match des **yara-rules**.
+
 
 Tous les fichiers non conformes seront supprimés sans confirmation.  
 
@@ -25,109 +33,47 @@ Tous les fichiers non conformes seront supprimés sans confirmation.
 
 Il vous suffit d'executer le scripts `install.sh` dans `scripts/`
 
-## Procédure d'installation manuelle :
-
-**Installation PyQt5**
-```
-$ sudo apt-get install build-essential checkinstall
-$ sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-$ sudo apt-get install python3-pyqt5  
-$ sudo apt-get install pyqt5-dev-tools
-$ sudo apt-get install qttools5-dev-tools
-```
-
----
-
-**Installation  librairies Python3**
-```
-pi@raspberrypi:$ pip3 install sip
-pi@raspberrypi:$ pip3 install --upgrade pip
-pi@raspberrypi:$ pip3 install requests
-```
-
----
-
-
-**Gestion des droits sur /media/pi**
-```
-pi@raspberrypi:$ cd /media
-pi@raspberrypi:/media $ sudo mkdir pi 
-pi@raspberrypi:/media $ sudo chmod a+rwx pi/
-```
-
----
-
-**Installation de l'application avec git**
-```
-cd /opt/
-sudo git clone https://github.com/Pr00filer/Code_Pr00filer.git
-sudo chown -R pi Code_Pr00filer
-```
-
----
-
-**Installation ClamScan avec le daemon**
-```
-sudo apt install clamav clamdscan clamav-daemon -y
-sudo killall freshclam
-sudo freshclam
-sudo service clamav-daemon start
-```
-
----
-
-**Création de la règle UDEV au montage de la clé**
-```
-sudo vim /etc/udev/rules.d/11-insert.rules
-```
-
-```
-ACTION=="add", SUBSYSTEM=="block", ENV{SYSTEMD_WANTS}+="insertUSB.service"
-```
-
-**Création de la règle UDEV au démontage de la clé**
-```
-sudo vim /etc/udev/rules.d/11-remove.rules
-```
-
-```
-ACTION=="remove", SUBSYSTEM=="block",RUN+="/opt/Code_Pr00filer/scripts/removeUSB.sh"
-```
-
-```
-sudo udevadm control --reload
-```
-
----
-
-**Création du service insertUSB.sh**
-```
-sudo vim  /etc/systemd/system/insertUSB.service
-```
-
-```
-[Unit]
-Description= When a USB stick is plugged
-
-[Service]
-Type=oneshot
-
-Environment=DISPLAY=:0
-ExecStart=/opt/Code_Pr00filer/scripts/insertUSB.sh
-
-[Install]
-WantedBy=multi-user.target
-```
-
-```
-sudo systemctl enable insertUSB.service
-sudo service insertUSB start
-```
-
----
-
 **Autoriser Montage automatique des clés USB**
 Allez dans les préférences de l'explrateur de fichiers:
 - Cochez seulement le montage automatique.
 
+## Config
 
+Un fichier de configuration est présent pour la config de base de l'application.
+
+Il est présent dans : **Code_Pr00filer/config**  
+Toutes les informations présentes sont les options par défaut.  **Les modifiers peut compromettre le fonctionnement de l'application pour le moment**
+
+Il contient :
+- **VERSION** = La version de l'application (pas utilisé)
+- **PATH_SOURCE** = Le chemin absolue vers l'application 
+- **PATH_SCAN** = Chemin où la clé USB est analysée.
+- **NAME_USER** = Nom de l'utilisateur principal (pas utilisé)
+- **ADDR_SERVER** = Contient l'adresse sur serveur master
+- **NAME_ADMIN** = Contient le nom de l'utilisateur qui va permettre l'envoie des données dans la Base de données du master.
+- **FILE_ADMIN_HASH** =  Chemin vers le fichier qui contient le hash pour l'envoie dans la base de données du master.
+- **FILE_CONFIG_HASH** = Contient le hash pour l'authentification de la requête qui est reçu par la raspberry par le serveur.
+- **FILE_VIRUS_HASH** = Chemin vers le fichier qui contient les hash des virus utilisé par le module check_hash.
+- **CHECK_VIRUSTOTAL_SCRIPT** =  Indique si le module VT est activé (pas utilisé)
+- **CHECK_EXTENSIONS_SCRIPT** = Indique si le module Extensions est activé (pas utilisé)
+- **DATA_EXTENSIONS_DELETE** = Indique les extensions supprimées par le module check_extensions.
+- **YARA_RULES_PATH** = Chemin où se trouvent les yara-rules
+
+## Serveur Raspberry
+La raspberry possède un mini serveur python qui sert à récupérer les nouvelles configuration du serveur master.  
+Pour le moment, ce serveur récupère et change les options:
+- **ADDR_SERVER**
+- **DATA_EXTENSIONS_SCRIPT**
+- Met à jour les hash présents dans **FILE_VIRUS_HASH**
+
+Le serveur python (server.py) est executé au démarrage de la station et toune en écoute sur le port 8000.   
+Il ne reçois les requêtes POST qui contiennent le hash présent dans **FILE_CONFIG_HASH**. Ce hash est unique pour chaque raspberry.  
+
+## Chiffrement
+Les communications avec le serveur et la raspberry utilise du chiffrement symétrique AES.
+
+## TO-DO :
+[ ] Revoir le script d'installation rapsberry  
+[ ] Revoir l'optimisation des modules (dossiers modules)
+[ ] Avoir la possibilité d'ajouter ou supprimer des modules facilement
+[ ] Revoir la personnalisation du fichier de config(avec install)
--- main.py
+++ main.py
@@ -40,6 +40,7 @@ class Worker(QRunnable):
     def run(self):
         self.fn(*self.args, **self.kwargs)
 
+## ------ Main Window ProgressBar ------ ##
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -47,6 +48,8 @@ class MainWindow(QMainWindow):
         self.threadpool = QThreadPool()
 
         self.id = 0
+
+        ## Pictures for prevention ##
         self.picturesDirectory = pathCORE+"/pictures/"
         self.images = ["picture1.png", "picture2.png", "picture3.png", "picture4.png"]
 
@@ -70,12 +73,19 @@ class MainWindow(QMainWindow):
         timerPicturesSlide.start(5000)
 
     def location_on_the_screen(self):
+        """
+           Description : Set the location of the main window on the screen. 
+        """
         qtRectangle = self.frameGeometry() 
         centerPoint = QDesktopWidget().availableGeometry().center()
         qtRectangle.moveCenter(centerPoint)
         self.move(qtRectangle.topLeft())
     
     def updatePicturesSlide(self):
+        """
+            Description :Init the pictures on the mainWindow and update them.
+            This also set the size of the pictures.
+        """
         if self.id < len(self.images):
             pixmap = QPixmap(self.picturesDirectory + self.images[self.id])
             pixmap2 = pixmap.scaled(800,550)
@@ -89,22 +99,27 @@ class MainWindow(QMainWindow):
         self.id = self.id + 1
 
     def createProgressBar(self):
+        """
+        Description :
+            - Create the ProgressBar
+            - Init the range value of the ProgressBar : 0 to 100
+            - Init the start value at 0
+        """
         self.progressBar = QProgressBar()
         self.progressBar.setRange(0, 100)
         self.progressBar.setValue(0)
 
-
-    def bash_command(cmd):
-        subprocess.Popen(cmd, shell=True, executable='/bin/bash')
-
-
     def scan(self):
+        """
+        Description : This function start all the modules for the analysis of the USB device.
+        This is the list of all the modules for now :
+        - check_extensions : Delete all the file that latch the unauthorized extensions
+        - check_hash : Delete all the file that match the virus hash list in the virus_hash.txt file.
+        - check_clamav : Start a clamdscan in the USB devise. Return the clamab report and delete the virus.
+        - check_virustotal : Get the hash of the file and then check if there is a result in VT using request. WARNING This modules can be limited because we are not using any API Token !
+
+        """
         self.progressBar.setValue(10)
-        # LEs extensions sont d'abord supprimées
-        # Ensuite les hash des fichiers qui sont dans notre hash_virus lists sont supprimés aussi
-        # Ensuite un scan CLAMAV
-        # Ensuite un scan VT
-        # Et pour finir, un scan YARA
 
         ##### ----- EXTENSIONS ----- #####
         check_ext.main_checkExt()  
@@ -117,11 +132,12 @@ class MainWindow(QMainWindow):
         option = "notdel"
         check_clamav.main_clamav(option)
         self.progressBar.setValue(40)
-        # Parse virus from previous scan to json
+        # Parse virus from previous scan to json (So that way we can get the hash before the file is deleted)
         data_json = check_clamav.clamav_virus_json()
         option = "del"
         check_clamav.main_clamav(option)
-        # On sauvegarde le JSON pour le récupérer dans les autres fonctions de l'application
+
+        # We save the JSON so we can use it everywhere in the application code
         with open(pathCORE+"/logs/data_json.json",'w') as outfile:
             json.dump(data_json, outfile)
 
@@ -133,7 +149,7 @@ class MainWindow(QMainWindow):
         check_yara.main_yara_check()
         self.progressBar.setValue(100)
             
-    ## Affichage deuxième fenêtre pour le report.log
+    ## Display the seconWindow with the report
         self.dialog = Second(self)
         self.dialog.show()
 
@@ -146,62 +162,49 @@ class Second(QMainWindow):
         super(Second, self).__init__(parent)
         self.setFixedSize(800,550)
         
-        # Label Temps du scan
+        # Label Scan Time
         self.time = QLabel(self)
         self.time.move(10,10)
         
-        # Label Nombre de virus
+        # Label Number Virus
         self.virus = QLabel(self)
         self.virus.move(10,35)
 
-        # Label Nombre d'erreurs
+        # Label Number Error
         self.errors = QLabel(self)
         self.errors.move(10,45)
-
-        # Label Nombre de fichiers analysés
-        #self.nbfile =  QLabel(self)
-        #self.nbfile.move(10,55)
-
-        # Label pour afficher la liste des Virus
-        #self.viruslist = QLabel(self)
-        #self.viruslist.move(10,65)
-       
-        # Label pour afficher la liste VIRUS TOTAL
-        #self.virustotal = QLabel(self)
-        #self.virustotal.move(10,100)
-
-        # Label pour afficher la liste des EXTENSIONS
-        #self.extensions = QLabel(self)
-        #self.extensions.move(10,200)
             
-        # Label image Warning/ok
+        # Label Finale picture OK / NOPE
         self.warning = QLabel(self)
         self.warning.move(0,0)
         self.Display()
 
-    # Fonction qui permet d'afficher le rapport final
     def Display(self):
-        # On récupère les données du json sauvegardé pendant le scan.
-        # data_json = Le JSON principal qui va etre utilisé pour le rapport.
+        """
+        Description : This is where most of the parsing is done. 
+        We will parse the reports of the modules to adapt them in our final report that will be sent to the server.
+        """
+        # We get the data in the json saved above
+        # data_json = The main JSON that will be used for the report.
         data_json = {}
         with open(pathCORE+"/logs/data_json.json","r") as json_file:
             data_json = json.load(json_file)
 
-        # Ouverture du log history
+        # The history.log file will be open during the whole function in order to place data in it
         with open(pathCORE+"/logs/history.log","a") as report:
             nbErrors=0
             dt_string = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-            # Array utilisé pour stocké les virus trouvé pour les logs 
+            # Array used to store viruses found in the logs 
             #   ex  : [VT] virus  /path/to/virus
             datareport_array = []
 
             ## INFO check_HASH #################################################
 
-            # On ouvre le JSON qui contient les virus toruvé dans le module check_hash
+            # We open the JSON that contains the viruses found in the check_hash module
             with open(pathCORE+"/logs/tmp_virus.json","r") as json_virus_file:
                 virus_json = json.load(json_virus_file)
-            #On ajoute les virus trouvé avec le module check_hash dans notre data_json final
+            # We add the viruses found with the check_hash module in our final data_json
             coutn_hash_virus = 0
             if not 'virus' in virus_json and len(virus_json['virus']) == 0:
                 pass
@@ -212,49 +215,55 @@ class Second(QMainWindow):
                     virus_path = virus['path']
                     virus_hash = virus['hash']
                     data_json['viruses'].append({'name':virus_name ,'hash':virus_hash})
-                    # On ajoute les virus trouvé dans le check_hash dans l'array qui va etre ajouté au rapport final
+                    # We add the viruses found in the check_hash in the array that will be added to the final report
                     datareport_array.append("[HC] "+virus_path+" "+virus_name+" "+virus_hash+"\n")
             
             ## Info VirusTotal #################################################
 
+            # Open the temporary log file of the virustotal result. 
             with open(pathCORE+"/logs/tmp_virustotal.log","r") as virustotal:
                 line_split = ""
-                count_vt = 0 # store the number of virus with VT
+                count_vt = 0 # This variable store the number of virus found with VT (history.log)
                 for line in virustotal:
-                    # Récup nom du virus + hashline_split = line.split()
                     # line = [0]:PATH, [1]:'-', [2]:SOURCE, [3]:':', [4]:NAME
                     if line[0] != "":
                         line_split = line.split(" ")
                         virus_name = line_split[4].strip() 
                         virus_path = line_split[0]
                         virus_hash = dataFunctions.get_sha256_hash(virus_path)
-                        # On ajoute le virus dans le JSON
+                        # Add the virus into the main json
                         data_json['viruses'].append({
                             'name':virus_name ,'hash':virus_hash
                         })
-                        count_vt = count_vt + 1
+                        count_vt = count_vt + 1*
+                        # Add the virus into the history array
                         datareport_array.append("[VT] "+virus_path+" "+virus_name+" "+virus_hash+"\n")
                         
-                        # On supprime le fichier trigger par VT
+                        # If the file match VT , it is deleted
                         dataFunctions.delete_file(virus_path)
                         print("[VT] "+virus_path+" supprimé !")
                     else:
-                        print('')
-            ## Info Extensions #################################################
+                        # If the V report log is empty, we don't do anything
+                        pass
 
+            ## Info Extensions #################################################
+            # We open the temporary log file that shows the files deleted by the module extensions
             with open(pathCORE+"/logs/tmp_extensions.log","r") as extensions:
                 line_split = ""
                 nbExt = 0
                 for line in extensions:
                     if "Total_ext" in line:
+                        # This part get the number of file removed by the module.
+                        # We make a loop because the line which interests us is at the end of the file. 
+                        # At the beginning we will find the deleted extensions with the absolute path of the deleted file.
                         line_split = line.split(" ")
                         nbExt_str = line_split[2].strip()
                         nbExt = int(nbExt_str)
                         break
-                print("NB EXT = "+str(nbExt))
+                # Now that we have the number of deleted extensions, we can see if there are any.
                 if nbExt == 0:
-                    # Aucunes Extensions supprimées
-                    print('')
+                    # No extensions removed
+                    pass
                 else:
                     line_split = ""
                     for lines in extensions:
@@ -265,89 +274,94 @@ class Second(QMainWindow):
                             Ext_path = line_split[0].strip()
                             datareport_array.append("[EX] "+Ext_path+"\n")
             ### Rapport YARA #################################################
-
-            # On récupère les virus trouvé avec yara
+            # We recover the temporary json file generated by the module yara-rules. 
+            # It contains the files that have match on a yara-rule. 
             with open(pathCORE+"/logs/tmp_yara.json","r") as json_yara_file:
                 yara_json = json.load(json_yara_file)
 
+            # We retrieve the number of files deleted by the yara-rules. (history.log)
             nbYARA = len(yara_json['virus'])
 
+            # If there is no virus in the json, no match yara so we do nothing.
             if not 'virus' in yara_json and len(yara_json['virus']) == 0:
                 pass
             else:
+                # there are one or more viruses, we willlook through them and recover the name + the hash + the path of the file.
                 for virus in yara_json['virus']:
                     virus_name = virus['name']
                     virus_path = virus['path']
                     virus_hash = virus['hash']
                     data_json['viruses'].append({'name':virus_name ,'hash':virus_hash})
-                    # On ajoute les virus trouvé dans le check_hash dans l'array qui va etre ajouté au rapport final
+                    # Add the virus into the history array
                     datareport_array.append("[YR] "+virus_path+" "+virus_name+" "+virus_hash+"\n")
 
             ## Info clamav #################################################
-
+            # Here we retrieve the temporary file of the clamav report.
             with open(pathCORE+"/logs/tmp_clamav.log","r") as clamav:
-                # fichier
+                # We recover the number of files present in the USB key.
                 nbFiles = dataFunctions.getNumberOfFiles(pathUSB)
+               
+                # If there are virus into VT scan, we add them to the clamav counter viruses
+                counter = 0 + count_vt + coutn_hash_virus
 
-                counter = 0 + count_vt + coutn_hash_virus# If there are virus into VT scan, we add them to the clamav counter viruses
-                print("Counter AV = "+str(counter))
-                # Temps du scan
+               # We will browse the log file to get all the information we need.
                 for line in clamav:
                     line_split = ""
+                    # Scanning time
                     if "Time:" in line:
                         line_split = line.split(" ")
                         time_scan = line_split[1].strip()
                         time_float = float(time_scan)
                         duration = int(time_float)
                         duration = duration + duration + 3
-                # Nombre de virus    
+                # Number of viruses   
                     if "Infected" in line:
                         line_split = line.split(" ")
                         nbVirus = line_split[-1].strip()
                         nbVirus = int(nbVirus)+count_vt
-                # Récupération des virus
+                # If there is a virus, FOUND on the line
                     if "FOUND" in line:
                         line_split = line.split(" ")
                         counter = counter + 1
-                        # display exemple
-                        # -- 2 : /path/virus NAME
-                        virus_name = line_split[1] 
-                        virus_path = line_split[0]
-                        #parcourir json au lieu du raport pour récupérer les info clamav
-                        #virus_hash = dataFunctions.get_sha256_hash(virus_path)
+                        virus_name = line_split[1] # Name
+                        virus_path = line_split[0] # Path
+                         # Add the virus into the history array
                         datareport_array.append("[AV] "+virus_path+" "+virus_name+"\n")
-                # Nombre d'erreurs
+                # Number of errors
                     if "errors:" in line:
                         line_split = line.split(" ")
                         nbErrors_str = line_split[-1].strip()
                         nbErrors = int(nbErrors_str)
-                # Affichage des images Warning et OK pour l'état de la clé
+
+                # Since we store the viruses in our main json, we count them to get the total number 
                 nbVirus = len(data_json['viruses'])
                 nbFiles = nbFiles + nbVirus
-                
+
+                # The final image is displayed to indicate to the user whether the USB stick is infected or not
                 if int(nbVirus) != 0:
-                    pixmap = QPixmap(pathCORE+"/assets/attention.png")
+                    pixmap = QPixmap(pathCORE+"/assets/attention.png") # VIRUS
                     pixmap = pixmap.scaled(800,550)
                     self.warning.setPixmap(pixmap)
                     self.warning.setFixedSize(800,550)
                 else:
-                    pixmap = QPixmap(pathCORE+"/assets/scan_ok.png")
+                    pixmap = QPixmap(pathCORE+"/assets/scan_ok.png") # NO VIRUS
                     pixmap = pixmap.scaled(800,550)
                     self.warning.setPixmap(pixmap)
                     self.warning.setFixedSize(800,550)
                 
-                # On set les valeur des champs TEXT
+                # The values of the text fields for the report displayed on the screen are initialized. 
+                # WARNING :  with the images you can't see them.
+
                 self.time.setText("Temps : "+str(duration)+" secondes")
                 self.time.adjustSize()
                 self.virus.setText("Virus : "+str(nbVirus))
                 self.virus.adjustSize()
+                # Set the error label with there are errors
                 if nbErrors != 0:
                     self.errors.setText("Une erreur est survenue pendant l'analyse !")
                     self.errors.adjustSize()
 
-            
-
-            # GENERER LE REPORT
+            # Generate the report history.log
             report.write("########### Rapport du "+dt_string+" ###########\n")
             report.write("# USB\n")
             report.write("UUID = "+dataFunctions.getUUID()+"\n")  
@@ -359,20 +373,21 @@ class Second(QMainWindow):
             report.write("VirusTotal = "+str(count_vt)+"\n")
             report.write("Extensions = "+str(nbExt)+"\n")
             report.write("Yara match = "+str(nbYARA)+"\n")
+            # Put all the virus 
             for line in datareport_array:
                 report.write(line)
 
-            # Ajout des info dans JSON_list
-            data_json['nbFiles'] = int(nbFiles)
-            data_json['nbVirus'] = nbVirus
-            data_json['duration'] = duration
-            data_json['uuidUsb'] = dataFunctions.getUUID()
-            data_json['nbErrors'] = nbErrors
+            # We add the last values in the main JSON
+            data_json['nbFiles'] = int(nbFiles) # Number of files
+            data_json['nbVirus'] = nbVirus # Number of viruses
+            data_json['duration'] = duration # Scanning time
+            data_json['uuidUsb'] = dataFunctions.getUUID() # USB UUID
+            data_json['nbErrors'] = nbErrors # Number of errors
 
-            # Unmount de la clé USB avec le script umountUSB.sh
+            # Umount the USB key 
             rc = subprocess.call(pathCORE+"/scripts/umountUSB.sh")
 
-            # On envoie la request POST au serveur
+            # Send the JSON to the server
             dataFunctions.createRequest(data_json)
 
 if __name__ == '__main__':
